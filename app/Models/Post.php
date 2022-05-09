6f81959e253ac09d885bd8867aee0c85d64a3fc7
Bart van Wijk <Bartvanwijkzk@gmail.com>
2022-05-09 13:18:09 +0200

    caching

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -30,16 +30,18 @@ class Post
 
     public static function all()
     {
-        return collect(File::files(resource_path("posts")))
-            ->map(fn($file) => YamlFrontMatter::parseFile($file))
-            ->map(fn($document) => new Post(
-                $document->title,
-                $document->exerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            ))
-            ->sortByDesc('date');
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path("posts")))
+                ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn($document) => new Post(
+                    $document->title,
+                    $document->exerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))
+                ->sortByDesc('date');
+        });
     }
 
     public static function find($slug)
