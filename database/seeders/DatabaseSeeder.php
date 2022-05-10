e7763803b099ad138f77a5115cfbd0ee829049ab
Bart van Wijk <Bartvanwijkzk@gmail.com>
2022-05-10 15:28:55 +0200

    Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,56 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-
-         $user = User::factory()->create();
-
-         $personal = Category::create([
-            'name' => 'Personal',
-             'slug' => 'personal'
-         ]);
-
-         $family = Category::create([
-            'name' => 'Family',
-             'slug' => 'family'
-         ]);
-
-         $work = Category::create([
-            'name' => 'Work',
-             'slug' => 'work'
-         ]);
-
-         Post::create([
-             'user_id' => $user->id,
-             'category_id' => $family->id,
-             'title' => 'Mijn Familie Post',
-             'slug' => 'my-family-post',
-             'exerpt' => 'Lorem ipsum ofzo',
-             'body' => '<p>Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd</p> '
-         ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'Mijn Persoonlijke Post',
-            'slug' => 'my-personal-post',
-            'exerpt' => 'Lorem ipsum ofzo',
-            'body' => '<p>Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd</p> '
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'Mijn Werk Post',
-            'slug' => 'my-work-post',
-            'exerpt' => 'Lorem ipsum ofzo',
-            'body' => '<p>Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd Lorem ahwd pauiwld ia;wud oid;aiw uchs liuasp puwpa dhlaiwld alhlwd</p> '
+        $user = User::factory()->create([
+            'name' => 'Bart'
         ]);
 
 
+        Post::factory(5)->create([
+            'user_id' => $user->id
+        ]);
     }
 }
